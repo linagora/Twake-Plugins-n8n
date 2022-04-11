7105281593661e91d12c54ebb4527126f9faa21a
Romaric Mourgues <rmourgues@linagora.com>
2022-04-11 16:08:59 +0200

    Fix some stuff

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ n8n plugin for Twake
 ### Install
 
 ```
-cd server
-sudo docker run -dp 3001:3001 --restart unless-stopped -e SERVER_PREFIX='plugins/n8n' -e SERVER_PORT=3001 -e CREDENTIALS_ENDPOINT='https://canary.twake.app' n8n
+sudo docker build -t n8n .
+sudo docker run \
+  --restart unless-stopped \
+  -dp 3001:3001 \
+  -e SERVER_PORT=3001 \
+  -e SERVER_PREFIX='/plugins/n8n' \
+  -e CREDENTIALS_ENDPOINT='https://canary.twake.app' \
+  -e CREDENTIALS_ID='abcdef' \
+  -e CREDENTIALS_SECRET='some-twake-application-secret' \
+  -e N8N_JWT_SECRET='secret' \
+  -e N8N_ENDPOINT='myN8nInstall.com' \
+  n8n
 ```
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,17 @@ import config from "config";
 import { HookEvent, N8nBodyEvent, N8nHeadersEvent } from "./types";
 import { createKey, listChannels, sendN8nMessage } from "./events";
 
+const prefix =
+  "/" +
+  ((config.get("server.prefix") || "") as string).replace(/(^\/|\/$)/g, "");
+
 const app = express();
 app.use(express.json());
 
-app.use(
-  config.get("server.prefix") + "/assets",
-  express.static(__dirname + "/../assets")
-);
+app.use(prefix + "/assets", express.static(__dirname + "/../assets"));
 
 // Entrypoint for every events comming from Twake
-app.post(config.get("server.prefix") + "/hook", async (req, res) => {
+app.post(prefix + "/hook", async (req, res) => {
   const event = req.body as HookEvent;
 
   if (
@@ -26,20 +27,17 @@ app.post(config.get("server.prefix") + "/hook", async (req, res) => {
   res.send({ error: "Not implemented" });
 });
 
-app.post(config.get("server.prefix") + "/n8n/channel", async (req, res) => {
+app.post(prefix + "/n8n/channel", async (req, res) => {
   const event = req.headers as N8nHeadersEvent;
   return res.send(await listChannels(event));
 });
 
-app.post(
-  config.get("server.prefix") + "/n8n/actions/message/save",
-  async (req, res) => {
-    const event_body = req.body as N8nBodyEvent;
-    const event_headers = req.headers as N8nHeadersEvent;
+app.post(prefix + "/n8n/actions/message/save", async (req, res) => {
+  const event_body = req.body as N8nBodyEvent;
+  const event_headers = req.headers as N8nHeadersEvent;
 
-    return res.send(await sendN8nMessage(event_body, event_headers));
-  }
-);
+  return res.send(await sendN8nMessage(event_body, event_headers));
+});
 
 const port = config.get("server.port");
 app.listen(port, (): void => {
